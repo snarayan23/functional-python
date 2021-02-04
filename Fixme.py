434e9108ec1ec5a57dcdedaf04d57452987ace1f
snarayan23 <snarayan23@cmc.edu>
2021-02-03 22:23:05 -0800

    Fixme.py with code

--- Fixme.py
+++ Fixme.py
@@ -20,7 +20,7 @@ def evens(n):
     >>> evens(-1)
     []
     '''
-
+    return list(filter(lambda x: x%2 == 0, range(n)))
 
 def threes(n):
     '''
@@ -37,7 +37,7 @@ def threes(n):
     >>> threes(50)
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
-
+    return [x for x in range(n) if x%10 == 3] 
 
 def small_words(text):
     '''
@@ -72,7 +72,7 @@ def squares(n):
     >>> squares(10)
     [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     '''
-
+    return list(map(lambda x: x*x, range(n)))
 
 def lengths(strings):
     '''
@@ -85,3 +85,4 @@ def lengths(strings):
     >>> lengths(['this','is','a','test'])
     [4, 2, 1, 4]
     '''
+    return list(map(lambda x: len(strings[x]), range(len(strings))))

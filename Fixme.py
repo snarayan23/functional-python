34fe7df02a3729270de7a4c6bee2af2620c6da18
snarayan23 <snarayan23@cmc.edu>
2021-02-04 10:55:34 -0800

    Modified Fixme.py

--- Fixme.py
+++ Fixme.py
@@ -22,7 +22,7 @@ def evens(n):
     >>> evens(-1)
     []
     '''
-    return list(filter(lambda x: x % 2 == 0, range(n)))
+    return list(filter(lambda x: x % 2 == 0, range(n+1)))
 
 
 def threes(n):
@@ -41,7 +41,7 @@ def threes(n):
     >>> threes(50)
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
-    return [x for x in range(n) if x % 10 == 3]
+    return [x for x in range(n+1) if 3 in str(x)]
 
 
 def small_words(text):
@@ -79,7 +79,7 @@ def squares(n):
     >>> squares(10)
     [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     '''
-    return list(map(lambda x: x * x, range(n)))
+    return list(map(lambda x: x * x, range(n+1)))
 
 
 def lengths(strings):
